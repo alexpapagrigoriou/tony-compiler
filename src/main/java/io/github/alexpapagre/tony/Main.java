35ed51a858d04bc7bf89e935a19bd4a7c59841da
Alexandros Papagrigoriou <apapagrigoriou17@gmail.com>
2026-03-27 02:22:48 +0200

    Update exception output

--- src/main/java/io/github/alexpapagre/tony/Main.java
+++ src/main/java/io/github/alexpapagre/tony/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
         if (args.length > 1) {
-            System.err.println("Usage: tony");
-            System.err.println("Usage: tony [file]");
+            System.err.println("Usage: java -jar tony.jar <file>");
+            System.err.println("Usage: java -jar tony.jar");
             System.exit(1);
         }
 
@@ -37,7 +37,7 @@ public class Main {
             System.err.println(e.getMessage());
             System.exit(1);
         } catch (Exception e) {
-            System.err.println("Parser error: " + e.getMessage());
+            System.err.println("Unexpected error: " + e.getMessage());
             System.exit(1);
         }
     }
