f1984b38c08efd3d781cbe62708b42413ba96f8e
Alexandros Papagrigoriou <apapagrigoriou17@gmail.com>
2026-04-05 15:45:29 +0300

    Main handles parser's output and doesn't accept input from stdin

--- src/main/java/io/github/alexpapagre/tonycompiler/Main.java
+++ src/main/java/io/github/alexpapagre/tonycompiler/Main.java
@@ -4,35 +4,34 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import io.github.alexpapagre.tonycompiler.ast.Program;
+
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length > 1) {
-            System.err.println("Usage: java -jar tony.jar <file>");
-            System.err.println("Usage: java -jar tony.jar");
+        if (args.length != 1) {
+            System.err.println("Usage: java -jar tony-compiler.jar <file>");
             System.exit(1);
         }
 
-        Lexer lexer = null;
-        if (args.length == 1) {
-            if (!args[0].endsWith(".tony")) {
-                System.err.println("File must be a '.tony' file!");
-                System.exit(1);
-            }
-
-            try {
-                lexer = new Lexer(new FileReader(args[0]));
-            } catch (IOException e) {
-                System.err.println("Could not open file: " + args[0]);
-                System.exit(1);
-            }
-        } else {
-            lexer = new Lexer(new InputStreamReader(System.in));
+        if (!args[0].endsWith(".tony")) {
+            System.err.println("File must be a '.tony' file!");
+            System.exit(1);
         }
 
-        Parser parser = new Parser(lexer);
+        FileReader fileReader = null;
         try {
-            parser.parse();
+            fileReader = new FileReader(args[0]);
+        } catch (IOException e) {
+            System.err.println("Could not open file: " + args[0]);
+            System.exit(1);
+        }
+
+        Lexer lexer = new Lexer(fileReader);
+        Parser parser = new Parser(lexer);
+        Program program = null;
+        try {
+            program = (Program) parser.parse().value;
         } catch (LexerException e) {
             System.err.println(e.getMessage());
             System.exit(1);
