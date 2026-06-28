65cc6b57ab1b787fac5611e99a69d457e9f7d035
Alexandros Papagrigoriou <apapagrigoriou17@gmail.com>
2026-06-28 20:20:50 +0300

    Not creating instance of visitors in semantic analyzer

--- src/main/java/io/github/alexpapagre/tonycompiler/semantics/SemanticAnalyzer.java
+++ src/main/java/io/github/alexpapagre/tonycompiler/semantics/SemanticAnalyzer.java
@@ -3,16 +3,12 @@ package io.github.alexpapagre.tonycompiler.semantics;
 import io.github.alexpapagre.tonycompiler.ast.Program;
 
 public class SemanticAnalyzer {
-    private final ScopeChecker scopeChecker;
-    private final TypeChecker typeChecker;
 
     public SemanticAnalyzer() {
-        this.scopeChecker = new ScopeChecker();
-        this.typeChecker = new TypeChecker();
     }
 
     public void analyze(Program program) {
-        program.accept(scopeChecker);
-        program.accept(typeChecker);
+        program.accept(new ScopeChecker());
+        program.accept(new TypeChecker());
     }
 }
