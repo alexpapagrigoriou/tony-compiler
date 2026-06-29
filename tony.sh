7f56f2527dd783dfeef7105a4797175b356ad260
Alexandros Papagrigoriou <apapagrigoriou17@gmail.com>
2026-06-29 21:46:46 +0300

    Update tony script to rename files and run renamed files

--- tony.sh
+++ tony.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-OUT=out
+OUT_DIR=out
 JAR=target/tony-compiler.jar
 
 usage() {
     echo "Usage:"
-    echo "  ./tony build <file.tony>"
-    echo "  ./tony run"
+    echo "  ./tony build <inputFile.tony> [-o <outputName>]"
+    echo "  ./tony run [<className>]"
     echo "  ./tony clean"
-    exit 1
 }
 
 CMD=$1
@@ -20,59 +19,71 @@ compile)
     if [ "$#" -ne 1 ]; then
         echo "Error: compile takes no arguments"
         usage
+        exit 1
     fi
     ./mvnw -q clean package
     ;;
 build)
-    if [ $# -ne 2 ]; then
-        echo "Error: build requires exactly 1 argument"
-        usage
-    fi
-
-    FILE=$2
-
-    if [ ! -f "$FILE" ]; then
-        echo "Error: file '$FILE' not found"
-        exit 1
-    fi
-
     if [ ! -f "$JAR" ]; then
         echo "Error: '$JAR' not found. Run './tony.sh compile' first."
         exit 1
     fi
 
-    rm -rf $OUT
-    mkdir -p "$OUT"
+    if [ $# -eq 2 ]; then
+        INPUT_FILE=$2
 
-    java -jar "$JAR" "$FILE"
+        java -jar "$JAR" "$INPUT_FILE"
+
+    elif [ $# -eq 4 ] && [ "$3" = "-o" ]; then
+        INPUT_FILE=$2
+        OUTPUT_NAME=$4
+
+        java -jar "$JAR" "$INPUT_FILE" -o "$OUTPUT_NAME"
+    else
+        usage
+        exit 1
+    fi
     ;;
 run)
-    if [ "$#" -ne 1 ]; then
-        echo "Error: run takes no arguments"
+    if [ ! -d "$OUT_DIR" ]; then
+        echo "Error: '$OUT/' not found. Run 'tony build <inputFile.tony> [-o <outputName>]' first."
+        exit 1
+    fi
+
+    CLASS_NAME="Main"
+
+    if [ $# -gt 2 ]; then
         usage
-    fi
-
-    if [ ! -d "$OUT" ]; then
-        echo "Error: out/ not found. Run 'tony build <file>' first."
         exit 1
     fi
 
-    if [ ! -f "$OUT/Main.class" ]; then
-        echo "Error: Main.class not found in out/"
-        exit 1
+    if [ -n "$2" ]; then
+        CLASS_NAME=$2
+
+        if ! [[ "$CLASS_NAME" =~ ^[A-Za-z][A-Za-z0-9]*$ ]]; then
+            echo "Error: invalid class name '$CLASS_NAME'"
+            echo "Output name must start with a letter and contain only letters and digits."
+            exit 1
+        fi
+
+        FIRST_CHAR=$(echo "${CLASS_NAME:0:1}" | tr '[:lower:]' '[:upper:]')
+        REST="${CLASS_NAME:1}"
+        CLASS_NAME="${FIRST_CHAR}${REST}"
     fi
 
-    java -cp "$OUT" Main
+    java -cp "$OUT_DIR" "$CLASS_NAME"
     ;;
 clean)
     if [ "$#" -ne 1 ]; then
         echo "Error: clean takes no arguments"
         usage
+        exit 1
     fi
 
-    rm -rf "$OUT"
+    rm -rf "$OUT_DIR"
     ;;
 *)
     usage
+    exit 1
     ;;
 esac
